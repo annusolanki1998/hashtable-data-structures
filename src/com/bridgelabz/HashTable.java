329b60af68d84f42dac70164d658c420653e72d5
Annu <annusinghraj1998@gmail.com>
2022-06-28 08:38:11 +0530

    ADDED :Find frequency of words in paragraph is counted and also printed

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -5,22 +5,19 @@ import java.util.Map;
 
 public class HashTable {
     public static void main(String[] args) {
-        System.out.println("Welcome to hash table program !");
-
+        System.out.println("Welcome to HashTable Program ");
         Hashtable<String, Integer> hashtable = new Hashtable<>();
-        String input = "To be or not to be";
+        String input ="Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
         String[] inputArray = input.toLowerCase().split(" ");
-        for (String string : inputArray) {
-            if (hashtable.containsKey(string)) {
-                hashtable.put(string, hashtable.get(string) + 1);
+        for (String str : inputArray) {
+            if (hashtable.containsKey(str)) {
+                hashtable.put(str, hashtable.get(str) + 1);
             } else {
-                hashtable.put(string, 1);
+                hashtable.put(str, 1);
             }
         }
         for (Map.Entry entry : hashtable.entrySet()) {
-            System.out.println(entry.getKey() + "  " + entry.getValue());
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
     }
 }
-
-
