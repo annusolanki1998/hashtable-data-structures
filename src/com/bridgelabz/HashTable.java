4142aabef80a4a0d6c1b6beb7e71bad3db82c9a7
Annu <annusinghraj1998@gmail.com>
2022-06-28 08:24:06 +0530

    ADDED :Find frequency of words in sentence is counted and also printed

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,7 +1,26 @@
 package com.bridgelabz;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class HashTable {
     public static void main(String[] args) {
         System.out.println("Welcome to hash table program !");
+
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        String input = "To be or not to be";
+        String[] inputArray = input.toLowerCase().split(" ");
+        for (String string : inputArray) {
+            if (hashtable.containsKey(string)) {
+                hashtable.put(string, hashtable.get(string) + 1);
+            } else {
+                hashtable.put(string, 1);
+            }
+        }
+        for (Map.Entry entry : hashtable.entrySet()) {
+            System.out.println(entry.getKey() + "  " + entry.getValue());
+        }
     }
 }
+
+
