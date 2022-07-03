76fd28730ca40cb3fc8537004e0e2d589489a82b
Annu <annusinghraj1998@gmail.com>
2022-07-03 11:42:47 +0530

    ADDED: Ability to remove avoidable word from paragraph

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -7,17 +7,18 @@ public class HashTable {
     public static void main(String[] args) {
         System.out.println("Welcome to HashTable Program ");
         Hashtable<String, Integer> hashtable = new Hashtable<>();
-        String input ="Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-        String[] inputArray = input.toLowerCase().split(" ");
+        String input = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        input = input.replace("avoidable","");     // removing avoidable word replace with space
+        String[] inputArray = input.toLowerCase().split(" ");  // It is converting to lowercase and also split it like every word
         for (String str : inputArray) {
-            if (hashtable.containsKey(str)) {
-                hashtable.put(str, hashtable.get(str) + 1);
+            if (hashtable.containsKey(str)) {    // checking every word
+                hashtable.put(str, hashtable.get(str) + 1);  // more than one time incremented by 1.
             } else {
-                hashtable.put(str, 1);
+                hashtable.put(str, 1); // only one time word in --> input
             }
         }
         for (Map.Entry entry : hashtable.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
+            System.out.println(entry.getKey() + " -> " + entry.getValue());// print the all frequency with value and string
         }
     }
 }
